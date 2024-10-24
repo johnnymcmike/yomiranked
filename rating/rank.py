622afc72e0947a6a1b58e6bb296ddafa6f4c3ac9
johnnymcmike <mbjmcm@gmail.com>
2024-10-24 15:44:47 -0400

    add elo minimum

--- rating/rank.py
+++ rating/rank.py
@@ -2,11 +2,19 @@ from elote import EloCompetitor
 
 # Returns a list of 2 ints. First one is winner's rating, second one is loser's rating.
 def CalculateRank(winnerRating, loserRating):
-    winner = EloCompetitor(initial_rating= winnerRating * 1.13)
-    loser = EloCompetitor(initial_rating= loserRating * 0.87)
+    winner = EloCompetitor(initial_rating= winnerRating * 1.09)
+    loser = EloCompetitor(initial_rating= loserRating * 0.83)
     winner.beat(loser)
+
+    winResult = winner.rating
+    loseResult = loser.rating
+
+    if winResult < 500:
+        winResult = 500
+    if loseResult < 500:
+        loseResult = 500
     
-    return [winner.rating, loser.rating]
+    return [winResult, loseResult]
 
 def WinProbability(p1rating, p2rating):
     p1 = EloCompetitor(initial_rating=p1rating)
