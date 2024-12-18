2e8d4346c6f744b47a4739a8667b07d7d16a94e4
johnnymcmike <mbjmcm@gmail.com>
2024-12-18 00:18:16 -0500

    send notif to discord bot for match reports

--- index.py
+++ index.py
@@ -135,6 +135,16 @@ def gamereport():
             winner.rating = round(newRatings[0])
             loser.rating = round(newRatings[1])
             knownMatch.save()
+
+            requests.post("http://localhost:8081/reportmatch", json=
+                          {"winnerName": winner.steamName, 
+                           "loserName": loser.steamName,
+                           "winnerEloBefore": knownMatch.winner_eloBefore,
+                           "loserEloBefore": knownMatch.loser_eloBefore,
+                           "winnerEloCurrent": knownMatch.winner_eloAfter,
+                           "loserEloCurrent": knownMatch.loser_eloAfter,
+                           "winnerSteamId": winner.steamId,
+                           "loserSteamId": loser.steamId,})
             
             winner.lastActive = datetime.datetime.now()
             loser.lastActive = datetime.datetime.now()
